0c99b8baef6ec65d5e15c37c386e8e30063f8d49
Krishi Tanna <tannakrishi1109@gmail.com>
2024-03-13 16:34:58 +0530

    test

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,14 +1,19 @@
 import userServices from "../services/user.services";
 import ErrorHandler from "../utils/errorHandler";
+import {StatusCodes} from "http-status-codes"
 
 const demo = (req, res, next) => {
   try {
-    const response = userServices.loginService();
+    const response = userServices.demoService();
 
     if (response.length < 5) {
       return next(new ErrorHandler("Error has occored", 420));
     }
-    res.send(response);
+    res.status(StatusCodes.OK).send({
+      success : true,
+      message : "This is demmo API!!",
+      result : response
+    });
   } catch (error) {
     console.error(error);
     res.status(500).send({
@@ -18,8 +23,17 @@ const demo = (req, res, next) => {
   }
 };
 
+const userRegister = (req, res, next) => {
+  try {
+    return next(new ErrorHandler("Data not found!", 404))
+  } catch (error) {
+    return next(new ErrorHandler("Something went wrong!", 500))
+  }
+};
+
 const userController = {
   demo,
+  userRegister,
 };
 
 export default userController;
